9d5eea40d3c1c40e6ddfe6dba6b6ae291ecf5eab
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-15 15:45:28 -0800

    created question objects

--- index.js
+++ index.js
@@ -2,7 +2,51 @@ const inquirer = require('inquirer');
 
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter application name (project title)'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter application description'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter application installation instructions'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter application usage instructions'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter application contribution guidelines'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter application test instructions'
+    },
+    {
+        type: 'input',
+        name: 'reporting',
+        message: 'Enter application error reporting process'
+    },
+    { 
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address'
+    },
+    {
+        type: 'input',
+        name: 'user',
+        message: 'Enter your Github username'
+    }
 ];
 
 // function to write README file
